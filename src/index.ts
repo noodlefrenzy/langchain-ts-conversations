39a2ec4c4616f80eeb8cdaa0f3b3612949298218
Michael Lanzetta <milanz@microsoft.com>
2023-04-11 14:46:13 +0900

    Adding simple turn-by-turn

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,63 @@
 import * as dotenv from "dotenv";
+import wrap from "word-wrap";
+
 import { OpenAI } from "langchain";
+import { ConversationChain } from "langchain/chains";
+import { BaseLLM } from "langchain/llms";
+import { PromptTemplate } from "langchain/prompts";
 
 dotenv.config();
+
+class Conv {
+  chain: ConversationChain;
+  lastResponse: string;
+
+  constructor(chain: ConversationChain, lastResponse: string) {
+    this.chain = chain;
+    this.lastResponse = lastResponse;
+  }
+}
 
 const model = new OpenAI({
   modelName: "gpt-3.5-turbo",
   openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0.8
 });
 
-const res = await model.call(
-  "What's a good idea for an application to build with GPT-3?"
-);
+async function prime_chain(model: BaseLLM, initial_prompt: string) {
+  const chain = new ConversationChain({ llm: model });
+  return new Conv(chain, (await chain.call({ input: initial_prompt })).response);
+}
 
-console.log(res);
+const person1 = { person: 'Noam Chomsky', details: 'renowned linguist' };
+const person2 = { person: 'James T. Kirk', details: 'captain of the USS Enterprise' };
+const num_turns = 2;
+
+const initial_prompt = new PromptTemplate({ template: "Please converse with me as if you are {person}, {details}. Say 'yes' if you agree.", inputVariables: ['person', 'details'] });
+
+const c1 = await prime_chain(model, await initial_prompt.format(person1));
+const c2 = await prime_chain(model, await initial_prompt.format(person2));
+
+async function logres(person: string, res: string) {
+  const wrappedRes = wrap(res, { indent: '  ', width: 100 });
+  console.log(person);
+  console.log(wrappedRes);
+}
+
+await logres(person1.person, c1.lastResponse);
+await logres(person2.person, c2.lastResponse);
+
+async function turn(chain1: Conv, chain2: Conv, conversation_starter?: string) {
+  const res1 = await chain1.chain.call({ input: conversation_starter ?? chain2.lastResponse });
+  const res2 = await chain2.chain.call({ input: res1.response });
+  await logres(person1.person, res1.response);
+  await logres(person2.person, res2.response);
+  chain1.lastResponse = res1.response;
+  chain2.lastResponse = res2.response;
+}
+
+let conversation_starter = 'What is the average airspeed velocity of an unladen swallow?';
+for (let i = 0; i < num_turns; i++) {
+  await turn(c1, c2, i === 0 ? conversation_starter : undefined);
+}
+console.log('=== Conversation complete ===')
